64e4696e13e887e755de6ed08aacf6a8736a8bec
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-08-30 16:53:12 +0530

    Updated checker.cs with Pure Functions

--- checker.cs
+++ checker.cs
@@ -5,18 +5,40 @@ namespace paradigm_shift_csharp
 class Checker
 {
     static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
+        return TemmperatureChecker(temperature) && SocChecker(soc) && ChargeRateChecker(chargeRate);
     }
+
+    public bool TemmperatureChecker(float temperature)
+    {
+       bool isInRange = temperature>=0 && temperature<=45;
+        if(!isInRange)
+        {
+            Console.WriteLine("Temperature is out of range!");
+        }
+        return isInRange;
+    }
+
+    public bool SocChecker(float soc)
+    {
+       bool isInRange = soc>=20 && soc<=80;
+        if(!isInRange)
+        {
+            Console.WriteLine("State of Charge is out of range!");
+        }
+        return isInRange;
+    }
+
+    public bool ChargeRateChecker(float chargeRate)
+    {
+        bool isInRange = chargeRate>=0.8;
+        if(!isInRange)
+        {
+            Console.WriteLine("Charge Rate is out of range!");
+        }
+        return isInRange;
+    }
+    
+    
 
     static void ExpectTrue(bool expression) {
         if(!expression) {
