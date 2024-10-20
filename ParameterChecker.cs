0d9403ac49a1eb709d2c9993c419e73891e85e99
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-10-20 23:09:41 +0530

    Create ParameterChecker.cs

---
+++ ParameterChecker.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace paradigm_shift_csharp
+{
+    public class ParameterChecker
+    {
+        private readonly IMessageLogger _logger;
+
+        public ParameterChecker(IMessageLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public bool ParameterInRange(float max, float value, float thresholdPercent, string errorMessage, float? min = null)
+        {
+            var thresholdNumber = CalculateThresholdNumber(thresholdPercent, max);
+            bool isInRange = CheckInRange(max, value, thresholdNumber, errorMessage, min);
+            if (!isInRange)
+                _logger.LogError($"{errorMessage} is out of range!");
+            return isInRange;
+        }
+
+        private bool CheckInRange(float max, float value, float thresholdNumber, string errorMessage, float? min = null)
+        {
+            if (min.HasValue)
+                return value >= min && value <= max;
+            else
+                return value <= max;
+        }
+
+        private float CalculateThresholdNumber(float thresholdPercent, float max)
+        {
+            return (max * (thresholdPercent / 100));
+        }
+
+        public bool IsOutOfThresholdRange(float value, float max, float thresholdNumber, float? min = null)
+        {
+            return (min.HasValue && value < min + thresholdNumber) || value > max - thresholdNumber;
+        }
+    }
+}
